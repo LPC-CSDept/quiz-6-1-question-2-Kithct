c0861fca7cd0701a9661575e2dd53bc325107965
Zelin Chen <6692czl@gmail.com>
2022-10-10 14:34:26 -0700

    q2(1)

--- q2.cpp
+++ q2.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 // ******************************
 // Your code here
+void getInput(int &n1, int &n2, int &n3);
+int  findMin(int n1, int n2, int n3);
+void printResult(int n1, int n2, int n3, int min);
+
+int main()
+{
+	int n1,n2,n3,min;
+	getInput(n1,n2,n3);
+	min=findMin(n1,n2,n3);
+	printResult(n1,n2,n3,min);
+}
 // ******************************
 
 // ******************************
@@ -11,4 +22,25 @@ using namespace std;
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
+}
+void getInput(int &n1, int &n2, int &n3)
+{
+	cout<<"Please Enter Three integer: "<<endl;
+	cin>>n1>>n2>>n3;
+}
+int  findMin(int n1, int n2, int n3)
+{
+	int min=n1;
+	if(n2<n1)
+	{
+		min=n2;
+		if(n3<n2)
+			min=n3;
+	}
+	else
+	{
+		if(n3<n1)
+			min=n3;
+	}
+	return min;
 }
